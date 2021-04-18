c48614a178a3bdcea327f8f55f9d02676cea5814
Anna Tielke <anna_tielke@gmx.de>
2021-04-18 17:37:22 +0200

    Iteration 1 and 2

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+require("../db");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+const Drone = require("../models/Drone.model.js");
+Drone.create(drones)
+  .then(() => {
+    console.log(`yay! ${drones.length} drones created`);
+    mongoose.connection.close();
+  })
+
+  .catch((err) => {
+    console.log("Error occured:", err);
+  });
